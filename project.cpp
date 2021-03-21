7e60d1ba5b5c0ef48e97de9020b3522e13a2935e
Nahid Hasan <nahidnub1126@gmail.com>
2021-03-22 02:03:28 +0600

    check this now

--- project.cpp
+++ project.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 void insertionSort(int arr[], int n)
 {
-	int i, key, j;
-	for (i = 1; i < n; i++)
-	{
-		key = arr[i];
-		j = i - 1;
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
-	cout<<"\n\nAccording to Insertion Sort, sorted data is: ";
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    cout<<"\n\nAccording to Insertion Sort, sorted data is: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void mergeSort(int arr[], int n)
+{
+
+}
+
+void quickSort(int arr[], int n)
+{
+
 }
 
 int main()
@@ -25,23 +35,40 @@ int main()
     int s;
     cout<<"Enter the size of array :";
     cin>>s;
-	int arr[s];
-	int n =s,option;
-	cout<<"Array Values are : ";
-	for(int i=0;i<s;i++){
+    int arr[s];
+    int n =s,option;
+    char ch;
+    cout<<"Array Values are : ";
+    for(int i=0; i<s; i++)
+    {
         cin>>arr[i];
-	}
-	cout<<"\nPress 1: Insertion Sort\nPress2: Merge Sort \nPress3: Quick Sort \nPress4: Exit\nChose Your Option==> ";
-	cin>>option;
-	switch(option){
-    case 1:
-        insertionSort(arr, n);
-        break;
-	}
+    }
+    do
+    {
+        cout<<"\nPress 1: Insertion Sort\nPress2: Merge Sort \nPress3: Quick Sort \nPress4: Exit\nChose Your Option==> ";
+        cin>>option;
+        switch(option)
+        {
+        case 1:
+            insertionSort(arr, n);
+            break;
+        case 2:
+            mergeSort(arr,n);
+            break;
+        case 3:
+            quickSort(arr,n);
+            break;
+        case 4:
+            exit(0);
+
+        }
+        cout<<"\nYou want more sort result y/n?";
+        cin>>ch;
+    }
+    while(ch=='y'||ch=='Y');
 
 
-
-	return 0;
+    return 0;
 }
 
 
