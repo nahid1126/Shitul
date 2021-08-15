2d8f1aac930e7ca485efc4e4d9d426c053a8eebf
Nahid Hasan <nahidnub1126@gmail.com>
2021-08-15 18:06:49 +0600

    mid code

---
+++ practice/src/Nahid/Main.java
@@ -0,0 +1,32 @@
+package Nahid;
+
+
+import java.util.Scanner;
+
+public class Main {
+//    void shalikPakhi(){
+//        System.out.println("Pakhew");
+//    }
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Pakhi obj = new Pakhi();
+        Pakhi obj1 = new Pakhi(158,"Tanzina");
+        Pakhi obj2 = new Pakhi(158,"Tanzina","Leading University",2222);
+
+//        Main ob=new Main();
+//        ob.shalikPakhi();
+//        Scanner input = new Scanner(System.in);
+//        String s;
+//        int n;
+//        s=input.nextLine();
+//        n=input.nextInt();
+//
+//        System.out.println(s+"\n"+n);
+
+
+    }
+
+}
---
+++ practice/src/Nahid/Pakhi.java
@@ -0,0 +1,34 @@
+package Nahid;
+
+public class Pakhi {
+    int n;
+    String s;
+    String str;
+
+    static int hasan;
+
+    public Pakhi() {
+        System.out.println("Hello");
+    }
+
+    public Pakhi(int n, String s) {
+        this.n = n;
+        this.s = s;
+        System.out.println("N= "+this.n+" S= "+this.s);
+    }
+    public Pakhi(int n, String s, String str,int hasan) {
+        this.n = n;
+        this.s = s;
+        this.str = str;
+        System.out.println("N= "+this.n+" S= "+this.s+" Str= "+this.str+" hasan= "+hasan);
+    }
+
+
+    //    static void display(){
+//        System.out.println("Hello");
+//    }
+//    void shitul(){
+//        System.out.println("Tanzina");
+//    }
+
+}
